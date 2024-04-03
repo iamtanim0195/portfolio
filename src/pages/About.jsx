a6b2cf69fe1fb318feffa836a91c3facda51a6f4
iamtanim0195 <sahadatmd195@gmail.com>
2024-04-04 00:32:17 +0600

    update about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -21,55 +21,50 @@ const About = async () => {
                 <h1>{data.name}</h1>
                 <ThemeSwitch />
             </div>
-            <div className='pt-10 flex flex-col'>
-                <div>
-                    <div className=' w-52 text-center mx-auto'>
-                        <p className='uppercase'> hello, my name is</p>
-                        <span className='text-4xl'>
-                            {data?.name.split(' ').map((word, index) => (
-                                <React.Fragment key={index}>
-                                    <span
-                                        className={index === 0 ? 'text-green-500' : ' '}
-                                        style={{ textShadow: '0 2px 5px rgba(0,0,0,0.5)' }}
-                                    >
-                                        {word}
-                                    </span>
-                                    {index !== data?.name.split(' ').length - 1 && <>&nbsp;</>}
-                                </React.Fragment>
-                            ))}
-                        </span>
-                        <p className={`text-3xl ${italianno.className}`}>i am {data?.title}</p>
-                    </div>
-
+            <div className=''>
+                <div className=' w-full text-center mx-auto'>
+                    <p className='uppercase'> hello, my name is</p>
+                    <span className='text-4xl'>
+                        {data?.name.split(' ').map((word, index) => (
+                            <React.Fragment key={index}>
+                                <span
+                                    className={index === 0 ? 'text-green-500' : ' '}
+                                    style={{ textShadow: '0 2px 5px rgba(0,0,0,0.5)' }}
+                                >
+                                    {word}
+                                </span>
+                                {index !== data?.name.split(' ').length - 1 && <>&nbsp;</>}
+                            </React.Fragment>
+                        ))}
+                    </span>
+                    <p className={`text-3xl ${italianno.className}`}>i am {data?.title}</p>
                 </div>
-                <div className='flex flex-col-reverse '>
-                    <div >
-                        <p className='text-center'>{data?.description}</p>
-                        <div className='text-black sm:text-center w-[75vw] mx-auto mt-3 bg-slate-200 p-1 rounded '>
-                            <p>Address: {data?.address}</p>
-                            <p>Phone Number: {data?.phoneNumber}</p>
-                            <p>Email: {data?.contactEmail}</p>
-                        </div>
-                    </div>
-                    <div>
-                        <Image
-                            src={(data.avatar.url) ? data.avatar.url : data?.alternateAvatars.url}
-                            width={500}
-                            height={500}
-                            alt={`Picture of the ${data?.name}`}
-                            className='rounded-full mx-auto'
-                        />
-                        <p className='w-[65vw] mx-auto p-2 bg-white text-sm text-black rounded-full shadow text-center '>{data?.quote}</p>
-                        <div className='flex gap-2 p-2 w-72 mx-auto'>
-                            <p className='bg-white text-sm text-black rounded-full shadow text-center w-40 '>
-                                <span>{data?.exp_year}</span> <span className='text-green-500'>+</span> YEARS OF <span className='text-green-500'>EXPERIENCE</span>
-                            </p>
-                            <p className='bg-white text-sm text-black rounded-full shadow text-center w-40'>
-                                <span>{data?.some_total}</span> COMPLETED  <span className='text-green-500'>PROJECTS</span>
-                            </p>
-                        </div>
-                    </div>
+            </div>
 
+            <div>
+                <Image
+                    src={(data.avatar.url) ? data.avatar.url : data?.alternateAvatars.url}
+                    width={500}
+                    height={500}
+                    alt={`Picture of the ${data?.name}`}
+                    className='rounded-full mx-auto'
+                />
+                <p className='w-[65vw] mx-auto p-2 bg-white text-sm text-black rounded-full shadow text-center '>{data?.quote}</p>
+                <div className='flex gap-2 p-2 w-72 mx-auto'>
+                    <p className='bg-white text-sm text-black rounded-full shadow text-center w-40 '>
+                        <span>{data?.exp_year}</span> <span className='text-green-500'>+</span> YEARS OF <span className='text-green-500'>EXPERIENCE</span>
+                    </p>
+                    <p className='bg-white text-sm text-black rounded-full shadow text-center w-40'>
+                        <span>{data?.some_total}</span> COMPLETED  <span className='text-green-500'>PROJECTS</span>
+                    </p>
+                </div>
+            </div>
+            <div className=''>
+                <p className='text-center'>{data?.description}</p>
+                <div className='text-black sm:text-center w-64 mx-auto mt-3 bg-slate-200 p-1 rounded '>
+                    <p>Address: {data?.address}</p>
+                    <p>Phone Number: {data?.phoneNumber}</p>
+                    <p>Email: {data?.contactEmail}</p>
                 </div>
             </div>
         </div>
